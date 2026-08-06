e5ff5f95e111128a31923215c16e12cece498c61
Roxy <75404941+TealSeer@users.noreply.github.com>
2026-08-06 17:00:07 -0400

    integrate imgui

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 #include <SFML/Graphics.hpp>
 #include "defines.h"
 #include "game.h"
+#include <imgui.h>
+#include <imgui-SFML.h>
 
 Game* game;
+bool show_debug_window = false;
 
 int terminate(sf::RenderWindow& window) {
 	window.close();
+	ImGui::SFML::Shutdown();
 	delete game;
 	return 0;
 }
@@ -14,6 +18,9 @@ int main()
 {
 	sf::RenderWindow window( sf::VideoMode( { WINDOW_WIDTH, WINDOW_HEIGHT } ), "Bugs", sf::Style::Close );
 	window.setFramerateLimit(60);
+	if (!ImGui::SFML::Init(window)) {
+		return -1;
+	}
 	sf::View scaledView(sf::Vector2f(BUFFER_WIDTH / 2, BUFFER_HEIGHT / 2), sf::Vector2f(BUFFER_WIDTH, BUFFER_HEIGHT));
 	window.setView(scaledView);
 	sf::RenderTexture buffer({ BUFFER_WIDTH, BUFFER_HEIGHT });
@@ -22,10 +29,12 @@ int main()
 	srand(time(0));
 	game->addBug(BUFFER_WIDTH / 2, BUFFER_HEIGHT / 2, sf::Color::White);
 
+	sf::Clock deltaClock;
 	while ( window.isOpen() )
 	{
 		while ( const std::optional event = window.pollEvent() )
 		{
+			ImGui::SFML::ProcessEvent(window, *event);
 			if (event->is<sf::Event::Closed>()) {
 				return terminate(window);
 			}
@@ -34,8 +43,7 @@ int main()
 					return terminate(window);
 				}
 				else if (keyPressed->scancode == sf::Keyboard::Scancode::Space) {
-					game->killall();
-					game->addBug(BUFFER_WIDTH / 2, BUFFER_HEIGHT / 2, sf::Color::White);
+					show_debug_window = !show_debug_window;
 				}
 			}
 		}
@@ -53,12 +61,24 @@ int main()
 			}
 		}
 
+		ImGui::SFML::Update(window, deltaClock.restart());
+		if (show_debug_window) {
+			ImGui::Begin("Debug", &show_debug_window, ImGuiWindowFlags_AlwaysAutoResize);
+			ImGui::Text("Total bugs: %d", game->bugsAlive);
+			if (ImGui::Button("Reset")) {
+				game->killall();
+				game->addBug(BUFFER_WIDTH / 2, BUFFER_HEIGHT / 2, sf::Color::White);
+			}
+			ImGui::End();
+		}
+
 		buffer.clear(sf::Color(16, 16, 16));
 		buffer.draw(pixels);
 		buffer.display();
 		window.clear(sf::Color(16, 16, 16));
 		sf::Sprite bufferSprite(buffer.getTexture());
 		window.draw(bufferSprite);
+		ImGui::SFML::Render(window);
 		window.display();
 	}
 }
